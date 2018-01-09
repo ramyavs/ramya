da178209b228502638408a406e10b9f240acca46
ramyavs <31505005+ramyavs@users.noreply.github.com>
2018-01-09 07:38:40 -0300

    Create jr.c

---
+++ jr.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main()
+{
+int a;
+printf ("enter the value ");
+scanf("%d,&a");
+if(a>0)
+{
+printf("the number is positive");
+else if(a<0)
+printf("the number is negative")
+}
+else 
+{
+printf("the number is zero");
+}
+}
