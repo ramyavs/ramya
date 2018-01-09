596883f5513a99ec037a314c78f34bb906ad96f8
ramyavs <31505005+ramyavs@users.noreply.github.com>
2018-01-09 08:08:18 -0300

    Update jr.c

--- jr.c
+++ jr.c
@@ -5,13 +5,9 @@ int a;
 printf ("enter the value ");
 scanf("%d,&a");
 if(a>0)
-{
 printf("the number is positive");
 else if(a<0)
 printf("the number is negative");
-}
 else 
-{
 printf("the number is zero");
-}
 }
